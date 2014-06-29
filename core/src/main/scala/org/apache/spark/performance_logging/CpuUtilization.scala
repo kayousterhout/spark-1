5fd296def4255343407db8b2d17bfef79f55caf4
Kay Ousterhout <kayousterhout@gmail.com>
2014-06-29 14:56:57 -0700

    Add per-task utilization information to metrics.
    This commit adds metrics to TaskMetrics describing the CPU, disk,
    and network utilization while the task was running.

---
+++ core/src/main/scala/org/apache/spark/performance_logging/CpuUtilization.scala
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.performance_logging
+
+object CpuUtilization {
+  // This is the correct value for most linux systems, and for the default Spark AMI.
+  val JIFFIES_PER_SECOND = 100
+}
+
+/**
+ * CPU utilization measures the average number of cores in use, so when the CPUs are fully
+ * utilized, the CPU utilization will be equal to the number of cores (i.e., it will typically
+ * be greater than 1).
+ */
+class CpuUtilization(startCounters: CpuCounters, endCounters: CpuCounters) extends Serializable {
+  val elapsedMillis = endCounters.timeMillis - startCounters.timeMillis
+  val elapsedJiffies = CpuUtilization.JIFFIES_PER_SECOND * (elapsedMillis * 1.0 / 1000)
+
+  val processUserUtilization =
+    ((endCounters.processUserJiffies - startCounters.processUserJiffies).toFloat / elapsedJiffies)
+  val processSystemUtilization =
+    ((endCounters.processSystemJiffies - startCounters.processSystemJiffies).toFloat /
+      elapsedJiffies)
+  val totalUserUtilization =
+    ((endCounters.totalUserJiffies - startCounters.totalUserJiffies).toFloat / elapsedJiffies)
+  val totalSystemUtilization =
+    ((endCounters.totalSystemJiffies - startCounters.totalSystemJiffies).toFloat / elapsedJiffies)
+
+  def this(startCounters: CpuCounters) = this(startCounters, new CpuCounters())
+}
--- core/src/main/scala/org/apache/spark/util/JsonProtocol.scala
+++ core/src/main/scala/org/apache/spark/util/JsonProtocol.scala
@@ -30,6 +30,8 @@ import org.json4s.jackson.JsonMethods._
 
 import org.apache.spark.executor.{DataReadMethod, InputMetrics, ShuffleReadMetrics,
   ShuffleWriteMetrics, TaskMetrics}
+import org.apache.spark.performance_logging.{BlockDeviceUtilization, CpuUtilization,
+  DiskUtilization, NetworkUtilization}
 import org.apache.spark.scheduler._
 import org.apache.spark.storage._
 import org.apache.spark._
@@ -241,6 +243,10 @@ private[spark] object JsonProtocol {
           ("Status" -> blockStatusToJson(status))
         })
       }.getOrElse(JNothing)
+    val cpuUtilization = taskMetrics.cpuUtilization.map(cpuUtilizationToJson).getOrElse(JNothing)
+    val diskUtilization = taskMetrics.diskUtilization.map(diskUtilizationToJson).getOrElse(JNothing)
+    val networkUtilization =
+      taskMetrics.networkUtilization.map(networkUtilizationToJson).getOrElse(JNothing)
     ("Host Name" -> taskMetrics.hostname) ~
     ("Executor Deserialize Time" -> taskMetrics.executorDeserializeTime) ~
     ("Executor Run Time" -> taskMetrics.executorRunTime) ~
@@ -252,7 +258,36 @@ private[spark] object JsonProtocol {
     ("Shuffle Read Metrics" -> shuffleReadMetrics) ~
     ("Shuffle Write Metrics" -> shuffleWriteMetrics) ~
     ("Input Metrics" -> inputMetrics) ~
-    ("Updated Blocks" -> updatedBlocks)
+    ("Updated Blocks" -> updatedBlocks) ~
+    ("Cpu Utilization" -> cpuUtilization) ~
+    ("Disk Utilization" -> diskUtilization) ~
+    ("Network Utilization" -> networkUtilization)
+  }
+
+  def cpuUtilizationToJson(cpuUtilization: CpuUtilization): JValue = {
+    ("Process User Utilization" -> cpuUtilization.processUserUtilization) ~
+    ("Process System Utilization" -> cpuUtilization.processSystemUtilization) ~
+    ("Total User Utilization" -> cpuUtilization.totalUserUtilization) ~
+    ("Total System Utilization" -> cpuUtilization.totalSystemUtilization)
+  }
+
+  def blockDeviceUtilizationToJson(blockDeviceUtilization: BlockDeviceUtilization): JValue = {
+    ("Disk Utilization" -> blockDeviceUtilization.diskUtilization) ~
+    ("Read Throughput" -> blockDeviceUtilization.readThroughput) ~
+    ("Write Throughput" -> blockDeviceUtilization.writeThroughput)
+  }
+
+  def diskUtilizationToJson(diskUtilization: DiskUtilization): JValue = {
+    diskUtilization.deviceNameToUtilization.map { diskNameAndUtil =>
+      diskNameAndUtil._1 -> blockDeviceUtilizationToJson(diskNameAndUtil._2)
+    }
+  }
+
+  def networkUtilizationToJson(networkUtilization: NetworkUtilization): JValue = {
+    ("Bytes Received Per Second" -> networkUtilization.bytesReceivedPerSecond) ~
+    ("Bytes Transmitted Per Second" -> networkUtilization.bytesTransmittedPerSecond) ~
+    ("Packets Received Per Second" -> networkUtilization.packetsReceivedPerSecond) ~
+    ("Packets Transmitted Per Second" -> networkUtilization.packetsTransmittedPerSecond)
   }
 
   def shuffleReadMetricsToJson(shuffleReadMetrics: ShuffleReadMetrics): JValue = {
