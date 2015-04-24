NetSys/spark-monotasks

Disk scheduler integration.
Refactored the BlockManager to be compatible with the new DiskScheduler
and DiskMonotasks. All interaction with the disk is now done via a
DiskMonotask. In order to get a version of this commit working as
quickly as possible, several features had to be left out. The
BlockManager no longer supports dropping blocks from memory to disk in
the event that memory fills up. The BlockManager also no longer supports
removing blocks from disk or replicating blocks to other BlockManagers.
The BlockManager methods for fetching blocks from remote BlockManagers
have been left in the code, but they should be removed as soon as
possible and replaced with logic that uses NetworkMonotasks.

The CacheManager has been removed and its functionality has been moved
into the RDD class. The logic the prevents two tasks from loading the
same block at the same time has been removed.

core/src/main/scala/org/apache/spark/monotasks/compute/RddComputeMonotask.scala
@@ -1,0 +1,40 @@
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.monotasks.compute
+
+import java.nio.ByteBuffer
+
+import org.apache.spark.{Partition, TaskContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.{RDDBlockId, StorageLevel}
+
+/**
+ * Computes the specified partition of the specified RDD and stores the result in the BlockManager.
+ * An RddComputeMonotask only needs to be used when the result of computing the RDD partition needs
+ * to be stored on disk.
+ */
+private[spark] class RddComputeMonotask[T](context: TaskContext, rdd: RDD[T], split: Partition)
+  extends ComputeMonotask(context) {
+
+  override def execute(): Option[ByteBuffer] = {
+    val blockId = new RDDBlockId(rdd.id, split.index)
+    val iterator = rdd.compute(split, context)
+    context.localDagScheduler.blockManager.cacheIterator(
+      blockId, iterator, StorageLevel.MEMORY_ONLY, true)
+    None
+  }
+}

kayousterhout/spark-1

Conflicts:
	core/src/main/scala/org/apache/spark/CacheManager.scala
	core/src/main/scala/org/apache/spark/Dependency.scala
	core/src/main/scala/org/apache/spark/broadcast/TorrentBroadcast.scala
	core/src/main/scala/org/apache/spark/monotasks/compute/ShuffleMapMonotask.scala
	core/src/main/scala/org/apache/spark/monotasks/disk/DiskReadMonotask.scala
	core/src/main/scala/org/apache/spark/monotasks/disk/DiskWriteMonotask.scala
	core/src/main/scala/org/apache/spark/monotasks/network/NetworkMonotask.scala
	core/src/main/scala/org/apache/spark/rdd/RDD.scala
	core/src/main/scala/org/apache/spark/shuffle/ShuffleReader.scala
	core/src/main/scala/org/apache/spark/storage/BlockManager.scala
	core/src/main/scala/org/apache/spark/storage/BlockManagerWorker.scala
	core/src/main/scala/org/apache/spark/storage/DiskBlockManager.scala
	core/src/main/scala/org/apache/spark/storage/DiskStore.scala
	core/src/main/scala/org/apache/spark/storage/MemoryStore.scala
	core/src/main/scala/org/apache/spark/storage/ThreadingTest.scala
	core/src/test/java/org/apache/spark/JavaAPISuite.java
	core/src/test/scala/org/apache/spark/CacheManagerSuite.scala
	core/src/test/scala/org/apache/spark/DistributedSuite.scala
	core/src/test/scala/org/apache/spark/monotasks/disk/DiskRemoveMonotaskSuite.scala
	core/src/test/scala/org/apache/spark/monotasks/disk/DiskWriteMonotaskSuite.scala
	core/src/test/scala/org/apache/spark/monotasks/network/NetworkMonotaskSuite.scala
	core/src/test/scala/org/apache/spark/rdd/PipedRDDSuite.scala
	core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
	core/src/test/scala/org/apache/spark/storage/BlockFileManagerSuite.scala
	core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
	streaming/src/main/scala/org/apache/spark/streaming/receiver/ReceiverSupervisorImpl.scala

core/src/main/scala/org/apache/spark/monotasks/compute/RddComputeMonotask.scala
@@ -1,0 +1,40 @@
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.monotasks.compute
+
+import java.nio.ByteBuffer
+
+import org.apache.spark.{Partition, TaskContextImpl}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.{RDDBlockId, StorageLevel}
+
+/**
+ * Computes the specified partition of the specified RDD and stores the result in the BlockManager.
+ * An RddComputeMonotask only needs to be used when the result of computing the RDD partition needs
+ * to be stored on disk.
+ */
+private[spark] class RddComputeMonotask[T](context: TaskContextImpl, rdd: RDD[T], split: Partition)
+  extends ComputeMonotask(context) {
+
+  override def execute(): Option[ByteBuffer] = {
+    val blockId = new RDDBlockId(rdd.id, split.index)
+    val iterator = rdd.compute(split, context)
+    context.localDagScheduler.blockManager.cacheIterator(
+      blockId, iterator, StorageLevel.MEMORY_ONLY, true)
+    None
+  }
+}