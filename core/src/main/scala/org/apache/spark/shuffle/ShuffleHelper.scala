e85ecdd1d12158f2ccf8d138d567d88755b7a46f
Kay Ousterhout <kayousterhout@gmail.com>
2015-04-27 14:52:14 -0700

    style fix

--- core/src/main/scala/org/apache/spark/shuffle/ShuffleHelper.scala
+++ core/src/main/scala/org/apache/spark/shuffle/ShuffleHelper.scala
@@ -69,11 +69,11 @@ class ShuffleHelper[K, V, C](
         blockIdToMapId(blockId) = index
         if (address.executorId == context.env.blockManager.blockManagerId.executorId) {
           localBlockIds += blockId
-          logInfo(s"KMZ: Created new ShuffleBlockId for local shuffle block "+
+          logInfo(s"KMZ: Created new ShuffleBlockId for local shuffle block " +
             s"${shuffleDependency.shuffleId}, map $index, reduce $reduceId")
         } else {
           val networkMonotask = new NetworkMonotask(context, address, blockId, size)
-          logInfo(s"KMZ: Created new ShuffleBlockId for remote shuffle block "+
+          logInfo(s"KMZ: Created new ShuffleBlockId for remote shuffle block " +
             s"${shuffleDependency.shuffleId}, map $index, reduce $reduceId")
           localBlockIds.append(networkMonotask.resultBlockId)
           fetchMonotasks.append(networkMonotask)
