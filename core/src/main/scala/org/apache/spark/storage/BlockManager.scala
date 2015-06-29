c60ef3cbce5ee9281a2a8021ba2e2390b4889164
Kay Ousterhout <kayousterhout@gmail.com>
2015-06-28 23:07:55 -0700

    Compile error

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -994,7 +994,7 @@ private[spark] class BlockManager(
    * Returns a Monotask that will load the specified block into the MemoryStore. Returns None if the
    * block is already in the MemoryStore or is not stored by this BlockManager.
    */
-  def getBlockLoadMonotask(blockId: BlockId, context: TaskContextImpl): Option[Monotask] = {
+  def getBlockLoadMonotask(blockId: BlockId, context: TaskContextImpl): Option[DiskReadMonotask] = {
     val info = blockInfo.get(blockId).orNull
     if (info != null) {
       info.synchronized {
