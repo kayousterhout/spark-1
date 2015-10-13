c47a0ff589510baad88a9e18a7494b26be020088
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-10-13 20:57:43 +0000

    Run more trials, warm up JAR copy, JVM etc.

--- examples/src/main/scala/org/apache/spark/examples/DrizzleBaseline.scala
+++ examples/src/main/scala/org/apache/spark/examples/DrizzleBaseline.scala
@@ -7,14 +7,28 @@ import org.apache.spark._
 
 object DrizzleBaseline {
   def main(args: Array[String]) {
+    val NUM_TRIALS = 5
     val conf = new SparkConf().setAppName("Drizzle Baseline")
     val sc = new SparkContext(conf)
     val slices = if (args.length > 0) args(0).toInt else 2
-    val depth = if (args.length > 1) args(1).toInt else 3
+    val depth = if (args.length > 1) args(1).toInt else 3    
     val n = math.min(1000000L * slices, Long.MaxValue).toInt // avoid overflow
-    val sum = sc.parallelize(1L to n, slices). map {i => 2L * i}.
-              treeReduce(_ + _, depth)
-    println("Sum is " + sum)
+
+    // Let all the executors join
+    Thread.sleep(5000)
+    // Warm up the JVM and copy the JAR out to all the machines etc.
+    sc.parallelize(0 until sc.getExecutorMemoryStatus.size,
+      sc.getExecutorMemoryStatus.size).foreach { x =>
+      Thread.sleep(10)
+    }
+
+    for (i <- 0 to NUM_TRIALS) {
+      val begin = System.nanoTime
+      val sum = sc.parallelize(1L to n, slices).map {i => 2L * i}.
+                treeReduce(_ + _, depth)
+      val end = System.nanoTime
+      println("Sum of " + n + " elements took " + (end - begin)/1e3 + " microseconds")
+    }
     sc.stop()
   }
 }
