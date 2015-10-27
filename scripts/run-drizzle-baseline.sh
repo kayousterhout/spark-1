e37afc2a3cd5771d3a0417767950474a7b3c9520
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-10-27 07:09:46 +0000

    Use --pdf-relative-paths to help rsync workflow

--- scripts/run-drizzle-baseline.sh
+++ scripts/run-drizzle-baseline.sh
@@ -7,13 +7,13 @@ SPARK_EVENTS_DIR=/mnt/spark-events
 
 #NUM_STAGES=6
 
-for NUM_STAGES in 4 6 7
+for NUM_STAGES in 7 #4 6
 do
   LOG_SUFFIX=`date +"%m_%d_%H_%M_%S"`
   $FWDIR/bin/run-example org.apache.spark.examples.DrizzleBaseline $NUM_PARTS $NUM_STAGES >& /mnt/drizzle-baseline-$NUM_PARTS-$NUM_STAGES-$LOG_SUFFIX.log
   APP_ID=`cat /mnt/drizzle-baseline-$NUM_PARTS-$NUM_STAGES-$LOG_SUFFIX.log | grep "Connected to Spark cluster with app ID" | awk '{print $NF}'`
   echo "For $NUM_PARTS $NUM_STAGES, $APP_ID finished. Parsing."
   pushd $FWDIR/scripts
-  python parse_logs.py $SPARK_EVENTS_DIR/$APP_ID | tail -n +12
+  python parse_logs.py --pdf-relative-path $SPARK_EVENTS_DIR/$APP_ID | tail -n +12
   popd
 done
