4b84ffeafdd6a31c30d80f25fefdf5abc5051218
Kay Ousterhout <kayousterhout@gmail.com>
2016-09-11 16:59:39 -0700

    Share each machine evenly across all task sets.
    With this change, the TaskSchedulerImpl tries to assign tasks to
    machines such that an equal number of tasks from each outstanding
    task set will be running on each machine. The motivation for this
    is to try to balance the resource use on each machine so that if,
    for example, a disk-intensive and CPU-intensive job are running
    concurrently, a given machine will have tasks from both jobs so
    that its resources remain fully utilized.  This is a heuristic to
    try to get good resource usage on all machines without adding
    a complex scheduling mechanism.`

--- core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
@@ -123,6 +123,15 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val actorSyste
           executorDataMap.get(executorId) match {
             case Some(executorInfo) =>
               executorInfo.freeSlots += scheduler.CPUS_PER_TASK
+              val taskSetId = scheduler.taskIdToTaskSetId(taskId)
+              val numRunningTasks = executorInfo.taskSetIdToRunningTasks(taskSetId)
+              if (numRunningTasks > 1) {
+                executorInfo.taskSetIdToRunningTasks.put(taskSetId, numRunningTasks - 1)
+              } else {
+                executorInfo.taskSetIdToRunningTasks.remove(taskSetId)
+              }
+              logInfo(s"After task $taskId completed on $executorId, " +
+                s"taskIdToRunningTasks is ${executorInfo.taskSetIdToRunningTasks}")
               makeOffers(executorId)
             case None =>
               // Ignoring the update since we don't know about the executor.
@@ -173,7 +182,8 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val actorSyste
           id,
           executorData.executorHost,
           executorData.freeSlots,
-          executorData.totalDisks)
+          executorData.totalDisks,
+          executorData.taskSetIdToRunningTasks)
       }.toSeq))
     }
 
@@ -185,7 +195,8 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val actorSyste
           executorId,
           executorData.executorHost,
           executorData.freeSlots,
-          executorData.totalDisks))))
+          executorData.totalDisks,
+          executorData.taskSetIdToRunningTasks))))
     }
 
     // Launch tasks returned by a set of resource offers
@@ -193,8 +204,8 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val actorSyste
       for (task <- tasks.flatten) {
         val ser = SparkEnv.get.closureSerializer.newInstance()
         val serializedTask = ser.serialize(task)
+        val taskSetId = scheduler.taskIdToTaskSetId(task.taskId)
         if (serializedTask.limit >= akkaFrameSize - AkkaUtils.reservedSizeBytes) {
-          val taskSetId = scheduler.taskIdToTaskSetId(task.taskId)
           scheduler.activeTaskSets.get(taskSetId).foreach { taskSet =>
             try {
               var msg = "Serialized task %s:%d was %d bytes, which exceeds max allowed: " +
@@ -211,6 +222,11 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val actorSyste
         else {
           val executorData = executorDataMap(task.executorId)
           executorData.freeSlots -= scheduler.CPUS_PER_TASK
+          val runningTasksOnExecutorForTaskSet =
+            executorData.taskSetIdToRunningTasks.getOrElse(taskSetId, 0)
+          executorData.taskSetIdToRunningTasks.put(taskSetId, runningTasksOnExecutorForTaskSet + 1)
+          logInfo(s"After task ${task.taskId} started on ${task.executorId}, " +
+            s"taskIdToRunningTasks is ${executorData.taskSetIdToRunningTasks}")
           executorData.executorActor ! LaunchTask(new SerializableBuffer(serializedTask))
         }
       }
